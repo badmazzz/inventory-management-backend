1c53636681b5c914560d662701b0ce875c398c4b
badmazzz <pmeshw12012004@gmail.com>
2024-06-29 22:07:17 +0530

    first commit

---
+++ src/models/order.models.js
@@ -0,0 +1,27 @@
+import mongoose from "mongoose";
+
+const orderSchema = new mongoose.Schema(
+  {
+    userID: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "users",
+      required: true,
+    },
+    ProductID: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "product",
+      required: true,
+    },
+    QuantityPurchased: {
+      type: Number,
+      required: true,
+    },
+    TotalPurchaseAmount: {
+      type: Number,
+      required: true,
+    },
+  },
+  { timestamps: true }
+);
+
+export const Order = mongoose.model("Order", orderSchema);
