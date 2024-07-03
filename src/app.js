d67a27a3ca32cd5e4c5145d5d19d8fcb09e11dc4
Meshv Patel <84256381+badmazzz@users.noreply.github.com>
2024-07-03 21:18:51 +0530

    Upload on render

--- src/app.js
+++ src/app.js
@@ -4,7 +4,7 @@ import cors from "cors";
 const app = express();
 
 const corsOptions = {
-  origin: "http://localhost:5173",
+  origin: "https://inventory-management-elvf.onrender.com",
   credentials: true,
 };
 
